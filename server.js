f0e030f6b00eea3470384cdb8eb9b4eff94f2512
loukazenska <loukazenska.thomas@gmail.com>
2026-05-05 19:54:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -178,9 +178,8 @@ app.post("/session/stop", (req, res) => {
 
   let content = `=== SESSION CAISSE ===\n`;
   content += `Date: ${new Date().toLocaleString()}\n\n`;
-  content += `Nombre de commande: ${sessionStats.totalSales}\n`;
+  content += `Nombre de ventes: ${sessionStats.totalSales}\n`;
   content += `Total: ${sessionStats.totalAmount.toFixed(2)} €\n\n`;
-
   content += `--- Détail produits ---\n`;
 
   for (const name in sessionStats.products) {
@@ -189,16 +188,7 @@ app.post("/session/stop", (req, res) => {
 
   content += `\n======================`;
 
-  const filename = `session_${Date.now()}.txt`;
-
-  fs.writeFileSync(filename, content);
-
-  res.json({ file: filename });
-});
-
-// ✅ AJOUT ICI
-app.get("/session/status", (req, res) => {
-  res.json({ active: sessionActive });
+  res.json({ content });
 });
 
 
